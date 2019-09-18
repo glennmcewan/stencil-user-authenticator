a70871815cf46860d9363cfb39272b080b9c04e1
Glenn McEwan <glenn@web-dev.ninja>
2019-09-18 23:30:43 +0000

    Add basic markup for the Login Form

--- src/components/login-form/login-form.tsx
+++ src/components/login-form/login-form.tsx
@@ -6,7 +6,21 @@ import { Component, h } from "@stencil/core";
 export class LoginForm {
   render() {
     return (
-      <div>I am the login form.</div>
+      <form>
+        <div class="form-group">
+          <label htmlFor="loginEmail">Email</label>
+          <input type="email" class="form-control" id="loginEmail" placeholder="Email address" />
+        </div>
+        <div class="form-group">
+          <label htmlFor="loginPassword">Password</label>
+          <input type="password" class="form-control" id="loginPassword" placeholder="Password" />
+        </div>
+        <div class="form-group form-check">
+          <input type="checkbox" class="form-check-input" id="rememberMe" />
+          <label class="form-check-label" htmlFor="rememberMe">Remember me</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Login</button>
+      </form>
     );
   }
 }
